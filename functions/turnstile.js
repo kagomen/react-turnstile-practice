275ac45d09ef74cf6aa804c0c888285b8bf2a619
kagome <fu.taz.1102@gmail.com>
2024-07-04 17:33:45 +0900

    add turnstile

--- functions/turnstile.js
+++ functions/turnstile.js
@@ -3,8 +3,34 @@ import { handle } from 'hono/cloudflare-pages'
 
 const app = new Hono()
 
-app.get('/turnstile', (c) => {
-  return c.text('turnstile test!')
+const verifyEndpoint = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
+
+app.post('/turnstile', async (c) => {
+  const { token } = await c.req.json()
+  console.log('token', token)
+  const secret = c.env.SECRET_KEY
+  console.log('secret', secret)
+
+  const res = await fetch(verifyEndpoint, {
+    method: 'POST',
+    body: `secret=${encodeURIComponent(secret)}&response=${encodeURIComponent(token)}`,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+  })
+
+  const data = await res.json()
+
+  return c.json(data)
+  // if (!data.success) {
+  //   return new Response('The provided Turnstile token was not valid! \n' + JSON.stringify(data))
+  // }
+
+  // // 検証成功時の処理
+  // // The Turnstile token was successfuly validated. Proceed with your application logic.
+  // // Validate login, redirect user, etc.
+  // // For this demo, we just echo the "/siteverify" response:
+  // return new Response('Turnstile token successfuly validated. \n' + JSON.stringify(data))
 })
 
 export const onRequest = handle(app)
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
+import { Turnstile } from '@marsidev/react-turnstile'
+import { useRef } from 'react'
+
 function App() {
+  const formRef = new useRef()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const formData = new FormData(formRef.current)
+    const token = formData.get('cf-turnstile-response')
+
+    // console.log('token', token)
+    // console.log('formData', formData)
+
+    // POSTメソッドでトークンをJSON形式でバックエンドに送信
+    const res = await fetch('/turnstile', {
+      method: 'POST',
+      body: JSON.stringify({ token }),
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+
+    console.log('res', res)
+
+    const data = await res.json()
+
+    console.log('data', data)
+    if (data.success) {
+      // 検証成功時の処理
+      console.log('Success!!!!!!!!!')
+    }
+  }
+
   return (
-    <p>hello</p>
+    <div>
+      <p>Tunrstile practice</p>
+      <form ref={formRef} onSubmit={handleSubmit}>
+        {/* <input type="text" /> */}
+        <Turnstile siteKey={import.meta.env.VITE_SITE_KEY} />
+        <button>submit</button>
+      </form>
+    </div>
   )
 }
 
